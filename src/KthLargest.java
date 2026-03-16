04cccabc8827735ea23a9496674c09d7233d6e5b
Sunil Kumar T <sunilkumarfsd12@gmail.com>
2026-03-16 17:31:39 +0530

    16th day Bi-weekly test

---
+++ src/KthLargest.java
@@ -0,0 +1,27 @@
+class KthLargest{
+
+    public static void main(String[] args) {
+        List<Integer> nums=Arrays.asList(3,2,1,5,6,4);
+        int k=2;
+//        Collections.sort(nums);
+//        System.out.println(nums.get(nums.size()-k));
+
+        while(k > 1){
+            int maxIdx=0;
+            int max=nums.get(0);
+            for(int i=1;i<nums.size();i++){
+                if(max<nums.get(i)){
+                    max=nums.get(i);
+                    maxIdx=i;
+                }
+            }
+            nums.set(maxIdx,0);
+            k--;
+        }
+        int max=0;
+        for(int n:nums){
+            max=Math.max(max,n);
+        }
+        System.out.println(max);
+    }
+}
