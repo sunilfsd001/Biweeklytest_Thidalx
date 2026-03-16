29044af7f76bf961c9f61f8039300d85eeb3ab7e
Sunil Kumar T <sunilkumarfsd12@gmail.com>
2026-03-16 17:33:56 +0530

    March 16th Bi-weekly test

--- src/KthLargest.java
+++ src/KthLargest.java
@@ -1,11 +1,9 @@
-class KthLargest{
+import java.util.*;
 
+class KthLargest{
     public static void main(String[] args) {
         List<Integer> nums=Arrays.asList(3,2,1,5,6,4);
         int k=2;
-//        Collections.sort(nums);
-//        System.out.println(nums.get(nums.size()-k));
-
         while(k > 1){
             int maxIdx=0;
             int max=nums.get(0);
@@ -23,5 +21,9 @@ class KthLargest{
             max=Math.max(max,n);
         }
         System.out.println(max);
+
+
+//        Collections.sort(nums);
+//        System.out.println(nums.get(nums.size()-k));
     }
 }
